fe0e87366530bc9131c284a189ae0b66d79ca642
Leandro <resileandro@gmail.com>
2022-10-31 19:52:50 -0300

    cartas

---
+++ productos.js
@@ -0,0 +1,161 @@
+const iphones = [
+
+  {
+  id: 1,
+  modelo: "iphone xr",
+  precio: "$155.000",
+  bateria: "2.942 mAh",
+  almacenamiento: "64GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_875020-MLA46545196917_062021-F.webp"
+ },
+
+  {
+  id: 2,  
+  modelo: "iphone 11",
+  precio: "$190.000",
+  bateria: "3.110 mAh",
+  almacenamiento: "128GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_724631-MLA46114990506_052021-F.webp"
+ },
+
+  {
+  id: 3,  
+  modelo: "iphone 12 pro max",
+  precio:  "$270.000",
+  bateria: "3.687 mAh",
+  almacenamiento: "256GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_614407-MLA46088610404_052021-F.webp"
+ },
+
+  {
+  id: 4,  
+  modelo: "iphone 13 mini",
+  precio: "$320.000",
+  bateria: "2.438 mAh",
+  almacenamiento: "256GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_873253-MLA47782477965_102021-F.webp"
+ },
+
+ 
+
+];
+
+const samsungs = [
+
+  {
+  id: 1,  
+  modelo: "samsung Galaxy S22 Ultra",
+  precio: "$264.999",
+  bateria: "2.942 mAh",
+  almacenamiento: "256GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_950182-MLA52132546719_102022-F.webp"
+ },
+
+  {
+  id: 2,  
+  modelo: "Samsung Galaxy S21 FE",
+  precio: "$174.999",
+  bateria: "4.500 mAh",
+  almacenamiento: "128GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_969567-MLA48800009129_012022-F.webp"
+ },
+
+  {
+  id: 3,  
+  modelo: "samsung Galaxy A53",
+  precio:  "$133.999",
+  bateria: "5.000 mAh ",
+  almacenamiento: "128GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_761209-MLA50262524021_062022-F.webp"
+ },
+
+  {
+  id: 4,  
+  modelo: "samsung Galaxy A32",
+  precio: "$73.999",
+  bateria: "5.000 mAh",
+  almacenamiento: "128GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_617385-MLA48697556229_122021-F.webp"
+ },
+
+];
+
+const motorolas = [
+
+ {
+  id: 1,  
+  modelo: "motorola Edge 30",
+  precio: "$78.999",
+  bateria: "4.030 mAh",
+  almacenamiento: "128GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_666783-MLA50693240691_072022-F.webp"
+ },
+
+  {
+  id: 2,  
+  modelo: "motorola Moto G22",
+  precio: "$45.999",
+  bateria: "5.000 mAh",
+  almacenamiento: "128GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_933206-MLA50262200380_062022-F.webp"
+ },
+
+  {
+  id: 3,  
+  modelo: "motorola Moto G71",
+  precio: "$65.999",
+  bateria: "5.000 mAh",
+  almacenamiento: "64GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_888008-MLA52132686498_102022-F.webp"
+ },
+
+  {
+  id: 4,  
+  modelo: "motorola moto e40",
+  precio:  "$40.999",
+  bateria: "5.000 mAh",
+  almacenamiento: "64GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_863352-MLA48464368267_122021-F.webp"
+ },
+
+];
+
+const xiaomis = [
+
+ {
+  id: 1,  
+  modelo: "xiaomi Redmi Note 11",
+  precio: "$78.999",
+  bateria: "5.000 mAh",
+  almacenamiento: "128GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_723360-MLA52140415844_102022-F.webp"
+ },
+
+  {
+  id: 2,  
+  modelo: "xiaomi Redmi 9A",
+  precio: "$52.999",
+  bateria: "5.000 mAh",
+  almacenamiento: "32GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_996743-MLA48765111834_012022-F.webp"
+ },
+
+  {
+  id: 3,  
+  modelo: "xiaomi Pocophone Poco M4 Pro",
+  precio: "$99.999",
+  bateria: "5.000 mAh",
+  almacenamiento: "256GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_704124-MLA52142912868_102022-F.webp"
+ },
+
+  {
+  id: 4,  
+  modelo: "xiaomi Mi 11T Pro ",
+  precio: "$215.000",
+  bateria: "5.000 mAh",
+  almacenamiento: "256GB",
+  foto: "https://http2.mlstatic.com/D_NQ_NP_2X_845010-MLA48328770801_112021-F.webp"
+ },
+
+];
--- script.js
+++ script.js
@@ -1,212 +1,86 @@
-const iphones = [
+const carrito = [];
+let contenedor = document.getElementById("celulares");
 
- {
-  modelo: "iphone 8",
-  precio: "85.000",
-  bateria: "1.821 mAh",
-  almacenamiento: "64GB"
- },
-
-  {
-  modelo: "iphone xr",
-  precio: "155.000",
-  bateria: "2.942 mAh",
-  almacenamiento: "64GB"
- },
-
-  {
-  modelo: "iphone 11",
-  precio: "190.000",
-  bateria: "3.110 mAh",
-  almacenamiento: "128GB"
- },
-
-  {
-  modelo: "iphone 12 pro max",
-  precio:  "270.000",
-  bateria: "3.687 mAh",
-  almacenamiento: "256GB"
- },
-
-  {
-  modelo: "iphone 13 mini",
-  precio: "320.000",
-  bateria: "2.438 mAh",
-  almacenamiento: "256GB"
- },
-
-];
-
-const samsungs = [
-
- {
-  modelo: "samsung Galaxy A22",
-  precio: "52.999",
-  bateria: "5.000 MAH",
-  almacenamiento: "64GB"
- },
-
-  {
-  modelo: "samsung Galaxy S22 Ultra",
-  precio: "264.999",
-  bateria: "2.942 mAh",
-  almacenamiento: "256GB"
- },
-
-  {
-  modelo: "Samsung Galaxy S21 FE",
-  precio: "174.999",
-  bateria: "4.500 mAh",
-  almacenamiento: "128GB"
- },
-
-  {
-  modelo: "samsung Galaxy A53",
-  precio:  "133.999",
-  bateria: "5.000 mAh ",
-  almacenamiento: "128GB"
- },
-
-  {
-  modelo: "samsung Galaxy A32",
-  precio: "73.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "128GB"
- },
-
-];
-
-const motorolas = [
-
- {
-  modelo: "motorola Edge 30",
-  precio: "78.999",
-  bateria: "4.030 mAh",
-  almacenamiento: "128GB"
- },
-
-  {
-  modelo: "motorola Moto G22",
-  precio: "45.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "128GB"
- },
-
-  {
-  modelo: "motorola Moto G71",
-  precio: "65.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "64GB"
- },
-
-  {
-  modelo: "motorola moto e40",
-  precio:  "40.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "64GB"
- },
-
-  {
-  modelo: "motorola Moto G82",
-  precio: "74.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "128GB"
- },
-
-];
-
-const xiaomis = [
-
- {
-  modelo: "xiaomi Redmi Note 11",
-  precio: "78.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "128GB"
- },
-
-  {
-  modelo: "xiaomi Redmi 9A",
-  precio: "52.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "32GB"
- },
-
-  {
-  modelo: "xiaomi Pocophone Poco M4 Pro",
-  precio: "99.999",
-  bateria: "5.000 mAh",
-  almacenamiento: "256GB"
- },
-
-  {
-  modelo: "xiaomi Mi 11 Lite",
-  precio:  "173.500",
-  bateria: "4.250 mAh",
-  almacenamiento: "128GB"
- },
-
-  {
-  modelo: "xiaomi Mi 11T Pro ",
-  precio: "215.000",
-  bateria: "5.000 mAh",
-  almacenamiento: "256GB"
- },
-
-];
-
-let preguntaProducto = prompt ("Ingrese el nombre del producto que quiera averiguar, si desea ver todos los productos en stock, escriba: ver todo ");
-
-
-function consultarProducto (){
-
-
-switch (preguntaProducto){
-
-  case "iphone":  iphones.forEach((iphone) => {console.log (iphone)});
-
-  break
-
-  case "samsung":  samsungs.forEach((samsung) => {console.log (samsung)});
-
-  break
-
-  case "motorola":  motorolas.forEach((motorola) => {console.log (motorola)});
-
-  break
-
-  case "xiaomi":  xiaomis.forEach((xiaomi) => {console.log (xiaomi)});
-
-  break
-
-  case "ver todo":iphones.forEach((iphone) => {console.log (iphone)}),
-                  samsungs.forEach((samsung) => {console.log (samsung)}),
-                  motorolas.forEach((motorola) => {console.log (motorola)}),
-                  xiaomis.forEach((xiaomi) => {console.log (xiaomi)});
-
-  break
-
+function cardIphone (){
+  for(const celular of iphones){
+    contenedor.innerHTML += `
+          <div class="card col-sm-3">
+            <img src=${celular.foto} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${celular.modelo} </h5>
+              <p class="card-text">${celular.bateria} <br> ${celular.almacenamiento} <br> ${celular.precio} </p>
+              <button id="btn${celular.id}" class="btn btn-primary">Comprar</button>
+            </div>
+          </div>`;
+  }
+ 
+  iphones.forEach((celular) =>{
+    document.getElementById(`btn${celular.id}`).addEventListener("click",function(){
+      agregarAlCarrito(celular);
+    });
+  });
+  
 }
 
+cardIphone ();
+
+function agregarAlCarrito(celularAComprar){
+   carrito.push(celularAComprar);
+   console.table(carrito);
 }
 
-consultarProducto();
+agregarAlCarrito ();
+
+function cardSamsung (){
+  for(const celular of samsungs){
+    contenedor.innerHTML += `
+          <div class="card col-sm-3" style= "">
+            <img src=${celular.foto} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${celular.modelo} </h5>
+              <p class="card-text">${celular.bateria} <br> ${celular.almacenamiento} <br> ${celular.precio} </p>
+              <button id="btn${celular.id}" class="btn btn-primary">Comprar</button>
+            </div>
+          </div>`;
+  }
+}
+
+cardSamsung ();
 
 
 
-const productoIphone = iphones.filter ((celular) => celular.modelo.includes(preguntaProducto));
 
-console.log (productoIphone);
+function cardMotorola (){
+  for(const celular of motorolas){
+    contenedor.innerHTML += `
+           <div class="card col-sm-3">
+            <img src=${celular.foto} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${celular.modelo} </h5>
+              <p class="card-text">${celular.bateria} <br> ${celular.almacenamiento} <br> ${celular.precio} </p>
+              <button id="btn${celular.id}" class="btn btn-primary">Comprar</button>
+            </div>
+          </div>`;
+  }
+}
 
-const productoSamsung = samsungs.filter ((celular) => celular.modelo.includes(preguntaProducto));
-
-console.log (productoSamsung);
-
-const productoMotorola = motorolas.filter ((celular) => celular.modelo.includes(preguntaProducto));
-
-console.log (productoMotorola);
-
-const productoXiaomi = xiaomis.filter ((celular) => celular.modelo.includes(preguntaProducto));
-
-console.log (productoXiaomi);
+cardMotorola ();
 
 
+
+
+function cardXiaomi (){
+  for(const celular of xiaomis){
+    contenedor.innerHTML += `
+           <div class="card col-sm-3">
+            <img src=${celular.foto} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${celular.modelo} </h5>
+              <p class="card-text">${celular.bateria} <br> ${celular.almacenamiento} <br> ${celular.precio} </p>
+              <button id="btn${celular.id}" class="btn btn-primary">Comprar</button>
+            </div>
+          </div>`;
+  }
+}
+
+cardXiaomi ();
