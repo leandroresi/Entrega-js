621e678c2651cd7f2acd31fe4478593c89f68706
Leandro <resileandro@gmail.com>
2022-11-13 15:24:14 -0300

    carrito

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const carrito = [];
+let carrito = [];
 let contenedor = document.getElementById("celulares");
+let btnFinalizar = document.getElementById("finalizarcompra");
+
+//funcion para mostrar la informacion de los productos agregados en el array
 
 function cardCelulares (){
   for(const celular of celulares){
     contenedor.innerHTML += `
-          <div class="card col-sm-3">
-            <img src=${celular.foto} class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${celular.modelo} </h5>
-              <p class="card-text">${celular.bateria} <br> ${celular.almacenamiento} <br> ${celular.precio} </p>
-              <button id="btn${celular.id}" class="btn btn-primary">Comprar</button>
+          <div class="caja-productos">
+            <img class="img-producto" src=${celular.foto} alt="...">
+            <div>
+              <h5>${celular.modelo} </h5>
+              <p>${celular.bateria} <br> ${celular.almacenamiento} <br> ${celular.precio} </p>
+              <button id="btn${celular.id}" class="btn btn-primary" style="margin:25px">Comprar</button>
             </div>
           </div>`;
   }
- 
+
+//agrega un evento al presionar el boton de los productos
+
   celulares.forEach((celular) =>{
     document.getElementById(`btn${celular.id}`).addEventListener("click", function (){
       agregarAlCarrito(celular);
@@ -24,25 +29,66 @@ function cardCelulares (){
 
 cardCelulares ();
 
+//funcion para mandar los productos al carrito y mostrarlos
+
 function agregarAlCarrito(celularAComprar){
-   carrito.push(celularAComprar);
-   console.table(carrito);
-    alert("producto" + celularAComprar.modelo + "agregado al carrito!!");
+  carrito.push(celularAComprar);
+  console.table(carrito)
+  //alert que muestra el producto agregado al carrito
+  Swal.fire({
+  title: `${celularAComprar.modelo}`,
+  text: 'Agregado al carrito',
+  imageUrl: `${celularAComprar.foto }`,
+  imageWidth: 250,
+  imageHeight: 250,
+  imageAlt: `imagen de : ${celularAComprar.modelo}`,
+})
    document.getElementById("carrito-productos").innerHTML += 
    ` 
-   <div class="card col-sm-2">
-            <img src=${celularAComprar.foto} class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${celularAComprar.modelo} </h5>
-              <p class="card-text">${celularAComprar.bateria} <br> ${celularAComprar.almacenamiento} <br> ${celularAComprar.precio} </p>
-            </div>
-          </div>
+    <tr>
+       <td>${celularAComprar.id}</td>
+       <td>${celularAComprar.modelo}</td>
+       <td>${celularAComprar.precio}</td>
+        <td> <i class='bx bx-trash-alt' ></i></td>
+    </tr>
    `
+   //muestra las caracteristicas de los objetos en el local storage
+
    localStorage.setItem("carrito",JSON.stringify(carrito))
+
+   let totalCarrito = carrito.reduce((acumulador,celu) => acumulador + celu.precio,0);
+   document.getElementById("totalCarrito").innerText = "Total a pagar $: "+totalCarrito;
  
 }
 
-agregarAlCarrito ();
 
 
+btnFinalizar.onclick = () => {
+  carrito = [];
+   document.getElementById("carrito-productos").innerHTML = "";
+   document.getElementById("totalCarrito").innerText = "";
 
+  const { value: email } = Swal.fire({
+  title: 'Ingresa tu email para enviarte los datos de tu compra',
+  input: 'email',
+  inputLabel: '',
+  inputPlaceholder: 'Ingresa tu email'
+})
+
+if (email) {
+  Swal.fire(`Entered email: ${email}`)
+}
+
+}
+setTimeout(() =>{
+  Toastify({
+
+text: "Informacion enviada, gracias por su compra",
+
+duration: 4000,
+
+style: {
+  background: "#CC3636"
+},
+
+}).showToast();}, 9000);
